4022f28443bfbb965fa9c4d04d65d4e4d1aee36a
unknown <komalkhalkar2255@gmail.com>
2024-05-29 14:55:04 +0530

    update file

--- index.js
+++ index.js
@@ -2,18 +2,18 @@ var Express=require("express");
 var Mongoclient=require("mongodb").MongoClient;
 var cors=require("cors");
 const multer=require("multer");
-
+const env=require("dotenv");
+env.config();
 var app=Express();
 app.use(cors());
 
-var CONNECTION_STRING="mongodb+srv://komalkhalkar:Komal%40123@cluster0.6nzkkrg.mongodb.net/todoappdb?retryWrites=true&w=majority&appName=Cluster0";
 
 
 var DATABASENAME="todoappdb";
 var database;
 
 app.listen(5038,()=>{
-    Mongoclient.connect(CONNECTION_STRING,(error ,client)=>{
+    Mongoclient.connect(process.env.CONNECTION_STRING,(error ,client)=>{
 
         database=client.db(DATABASENAME);
         console.log("Mongo DB connection successful");
